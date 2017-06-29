9bb70b99a34d18ccd1cf8239d389028b1481e697
Nikolay <nikolai.komarov@optibet.ee>
2017-06-29 12:41:21 +0300

    add eslint traiiling comma rule (comma-dangle)

--- rules/node.js
+++ rules/node.js
@@ -1,14 +1,21 @@
 'use strict';
 
 module.exports = {
-  env: {
-    node: true
-  },
-  extends: [
-    'hapi'
-  ],
-  rules: {
-    'no-undef': ['error'],
-    'hapi/hapi-capitalize-modules': 0
-  }
+    env: {
+        node: true,
+    },
+    extends: [
+        'hapi'
+    ],
+    rules: {
+        'no-undef': ['error'],
+        'hapi/hapi-capitalize-modules': 0,
+        'comma-dangle': ['error', {
+            'arrays': 'never',
+            'objects': 'always',
+            'imports': 'always',
+            'exports': 'always',
+            'functions': 'never',
+        }],
+    },
 };
