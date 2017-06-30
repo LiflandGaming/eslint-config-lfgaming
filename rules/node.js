af9ce5f1ece95d690be344ab299876f953187a05
Nikolay <nikolai.komarov@optibet.ee>
2017-06-30 11:17:06 +0300

    Never trailling comma for functions

--- rules/node.js
+++ rules/node.js
@@ -10,6 +10,12 @@ module.exports = {
     rules: {
         'no-undef': ['error'],
         'hapi/hapi-capitalize-modules': 0,
-        'comma-dangle': ['error', 'always-multiline'],
+        'comma-dangle': ['error', {
+            'arrays': 'always-multiline',
+            'objects': 'always-multiline',
+            'imports': 'always-multiline',
+            'exports': 'always-multiline',
+            'functions': 'never',
+        }],
     },
 };
